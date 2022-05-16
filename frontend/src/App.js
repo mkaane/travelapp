5c4adca7636a1f32d9436b29f8f7bb6d564ab269
mkaane <mkaan97@gmail.com>
2022-05-17 01:52:50 +0300

    small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
+import axios from "axios"
 import Map, {Marker, Popup} from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -7,12 +8,26 @@ import StarRateIcon from '@mui/icons-material/StarRate';
 import "./app.css"
 
 function App() {
-
+  const [pins, setPins] = useState([])
   const [viewport, setViewport] = useState({
     latitude: 39.925533,
     longitude: 32.866287,
     zoom: 6
   })
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+
+
   return (
   <div className="App">
   <Map
@@ -22,21 +37,27 @@ function App() {
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
     >
-      <Marker latitude={39.925533} longitude={32.866287} offsetLeft={-20} offsetTop={-10}>
+      
+      {pins.map(p => (
+        <>
+      <Marker latitude={p.lat} 
+      longitude={p.long} 
+      offsetLeft={-20} 
+      offsetTop={-10}>
       <LocationOnIcon />
       </Marker>
       
       <Popup
-        latitude={39.925533}
-        longitude={32.866287}
+        latitude={p.lat}
+        longitude={p.long}
         closeButton={true}
         closeOnClick={false}
         anchor="left">
           <div className='card'>
             <label>Place</label>
-            <h4 className='place'>Anıtkabir</h4>
+            <h4 className='place'>{p.title}</h4>
             <label>Review</label>
-            <p className='desc'>Beautiful Place, I like it</p>
+            <p className='desc'>{p.desc}</p>
             <label>Rating</label>
             <div className='stars'>
               <StarRateIcon className='star'/>
@@ -46,11 +67,13 @@ function App() {
               <StarRateIcon className='star'/>
             </div>
             <label>Information</label>
-            <span className='username'>Created by <b>kaan</b></span>
+            <span className='username'>Created by <b>{p.username}</b></span>
             <span className='date'>1 hour ago</span>
           </div>
       </Popup>
       
+      </>
+      ))}
     
     </Map>
     </div>
